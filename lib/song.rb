3f161600a4ce029e9fbf901ab3853942736f5785
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 02:58:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,9 @@ class Song
     hash = {}
     @@genres.each do |genre|
       if hash[genre]
-        hash[genre] = 1
-      else
         hash[genre] += 1
+      else
+        hash[genre] = 1
       end
     end
     hash
